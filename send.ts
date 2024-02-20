8b13bb4b27ebc60809ebbb846b4ab3e36eb85ca6
Hitesh Lalwni <lwni@protonmail.com>
2024-02-20 11:08:01 +0530

    working

--- send.ts
+++ send.ts
@@ -1,32 +1,98 @@
-import { networks, Psbt,  } from "bitcoinjs-lib";
+import { networks, Psbt,  address, crypto} from "bitcoinjs-lib";
 import ECPairFactory from "ecpair";
 import * as tinysecp from "tiny-secp256k1"
-const sender_WIF = "cS8bhzBso6M21MnigRtRUVPPgG9qSMnrcsoBPjzkw8Bch8La6JVd";
-const rec_pkh = "tb1q9cwthujqtjddj89q6f8ky9lec85z873t3msyn0";
+import mempoolJS from "@mempool/mempool.js";
+import axios from "axios";
 
 const testnet = networks.testnet;
 const ECPair = ECPairFactory(tinysecp);
-const keyPair = ECPair.fromWIF(sender_WIF, testnet);
 const utxo_txid = "ecdd8a83d153ae29dee52b59b207df29bea2972d052dd007330eca1a851c9b59";
 
+function getPKHfromPublicKey(publicKey: Buffer) {
+return address.toBech32(crypto.ripemd160(crypto.sha256(publicKey)),0, 'tb'); 
+}
 
-const transaction = new Psbt({network: testnet}); 
 
-transaction.addInput({
-  hash: utxo_txid,
-  index: 0,
-  witnessUtxo: {
-    script: Buffer.from("0014eda1bcb51d38cefa21f04fcba0e5c2e46097d84d", 'hex'),
-    value: 64613
+async function createTransaction(sender_WIF : string, rec_pkh: string, satoshis: number, fees :number) {
+
+const senderKeyPair = ECPair.fromWIF(sender_WIF, testnet);
+
+const senderPKH = (getPKHfromPublicKey(senderKeyPair.publicKey))
+console.log(senderPKH)
+  const { bitcoin: { addresses, transactions } } = mempoolJS({
+    hostname: 'mempool.space',
+    network: 'testnet'
+  });
+
+  const addressTxsUtxo = await addresses.getAddressTxsUtxo({ address: senderPKH });
+
+  console.log("utxo's");
+  console.log(addressTxsUtxo);
+
+  const transaction = new Psbt({network: testnet});
+
+  let tot_balance = 0;
+  let inputs = 0;
+
+  for(let utxo of addressTxsUtxo) {
+    let transaction_details = await transactions.getTx({txid: utxo.txid})
+    tot_balance += utxo.value;
+    inputs += 1;
+    console.log("adding input");
+    transaction.addInput({
+      hash: utxo.txid,
+      index: utxo.vout,
+      witnessUtxo: {
+        script: Buffer.from(transaction_details.vout[utxo.vout].scriptpubkey, 'hex'),
+        value: utxo.value
+      }
+    });
   }
-});
 
-transaction.addOutput({
-  address: rec_pkh,
- value: 5000,
-});
 
-transaction.signInput(0, keyPair);
-transaction.finalizeAllInputs();
-const signed_tx = transaction.extractTransaction().toHex();
-console.log('Signed Raw Transaction:', signed_tx);
+
+  transaction.addOutput({
+    address: rec_pkh,
+    value: satoshis
+  });
+
+  
+  let rem_balance = tot_balance - satoshis - fees;
+  if(rem_balance < 0) throw new Error("insufficient funds");
+  transaction.addOutput({
+    address: senderPKH,
+    value: rem_balance
+  })
+
+  for(let i = 0; i < inputs; i++) {
+    transaction.signInput(i, senderKeyPair);
+  }
+  transaction.finalizeAllInputs();
+  const signed_tx = transaction.extractTransaction().toHex();
+
+  console.log(signed_tx);
+  console.log("broadcasting...");
+
+  
+
+
+let txid = axios.request({
+  method: "POST",
+  maxBodyLength: Infinity,
+  url: 'https://mempool.space/testnet/api/tx',
+  headers: { 
+    'Content-Type': 'text/plain'
+  },
+  data: signed_tx
+})
+
+  console.log("sent!")
+  console.log(txid);
+
+  
+
+
+}
+
+
+createTransaction("cS8bhzBso6M21MnigRtRUVPPgG9qSMnrcsoBPjzkw8Bch8La6JVd", "tb1q9cwthujqtjddj89q6f8ky9lec85z873t3msyn0", 500, 2000);
